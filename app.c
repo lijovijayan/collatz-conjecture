6614808f89c62b773836398ab78874e713149afb
lijovijayan <lijovijayan00@gmail.com>
2020-07-27 20:11:54 +0530

    with hardcoded number

---
+++ app.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+unsigned long long int hailstone_sequence(unsigned long long int num)
+{
+    printf("%d, ", num);
+    if (num == 1)
+    {
+        return 1;
+    }
+    else if (num % 2 == 0)
+    {
+        return hailstone_sequence(num / 2);
+    }
+    else
+    {
+        return hailstone_sequence((3 * num) + 1);
+    }
+}
+void main()
+{
+    hailstone_sequence(45744786);
+}
