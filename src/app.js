6e71bb05fb8c684c881b74aba79b23fdfd6bc83f
Anoop Chandran <anoop.chandran@blueripples.com>
2020-07-31 22:18:39 +0530

    updated html, css & js files

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 var Chart = require("chart.js");
-var ctx = document.getElementById("myChart");
-colors = [
-  "rgba(255, 99, 132, 0.2)",
-  "rgba(54, 162, 235, 0.2)",
-  "rgba(255, 206, 86, 0.2)",
-  "rgba(75, 192, 192, 0.2)",
-  "rgba(153, 102, 255, 0.2)",
-  "rgba(255, 159, 64, 0.2)",
-];
-
+var ctx = document.getElementById("chart");
 function getRandomColor() {
   var letters = "0123456789ABCDEF";
   var color = "#";
